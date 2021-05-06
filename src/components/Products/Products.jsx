a6c90f2a9bee33c683c9c18bd4fbf321945843f6
unknown <ashiwn.menon202@gmail.com>
2021-05-06 14:26:26 +0530

    Products added

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import Product from './Product/Product'
 
+import useStyles from './styles'
 
-const products = [
-    {id:1,name:'Shoes',description:'Running shoes',price:'Rs2000',image:'https://images.unsplash.com/photo-1575537302964-96cd47c06b1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'},
-    {id:2,name:'Macbook',description:'Apple Macbook',price:'Rs90000',image:'https://images.unsplash.com/photo-1537498425277-c283d32ef9db?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1057&q=80'}
-]
 
-function Products() {
+// const products = [
+//     {id:1,name:'Shoes',description:'Running shoes',price:'Rs2000',image:'https://images.unsplash.com/photo-1575537302964-96cd47c06b1b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'},
+//     {id:2,name:'Macbook',description:'Apple Macbook',price:'Rs90000',image:'https://images.unsplash.com/photo-1537498425277-c283d32ef9db?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1057&q=80'}
+// ]
+
+function Products({products}) {
+    const classes = useStyles();
     return (
-        <div>
+        <div className={classes.content}>
+            <div className={classes.toolbar}/>
             <Grid container justify='center' spacing={4}>
             {products.map((product)=>(
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
